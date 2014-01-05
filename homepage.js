4efe983eeef013ed98877dc183dfc5eaaba45e29
kevinvincent <kevinsundar@gmail.com>
2014-01-05 11:39:03 -0800

    Added working social links

--- homepage.js
+++ homepage.js
@@ -72,5 +72,10 @@ $(document)
       $paneTarget.stop().scrollTo( '#contact', 400 );
     });
 
+    $('#mail').click(function() { window.location = "mailto:kevinsundar@gmail.com"; })
+    $('#email').click(function() { window.location = "mailto:kevinsundar@gmail.com"; })
+    $('#github').click(function() { window.location = "https://github.com/kevinvincent"; })
+    $('#linkedin').click(function() { window.location = "http://www.linkedin.com/profile/view?id=201989673"; })
+
   })
 ;
